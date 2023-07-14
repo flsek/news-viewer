547896142caeddee242a9316c426dc71189412fd
muchpretty99 <muchpretty99@ibizsoftware.net>
2023-07-14 10:29:35 +0900

    react-router-dom 적용

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
-import React, { useCallback, useState } from 'react';
-import NewsList from './components/NewsList';
-import Categories from './components/Categories';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import NewsPage from './pages/NewsPage';
 
 const App = () => {
-  const [category, setCategory] = useState('all');
-  const onSelect = useCallback((category) => setCategory(category), []);
-
   return (
-    <>
-      <Categories category={category} onSelect={onSelect} />
-      <NewsList category={category} />
-    </>
+    <Routes>
+      <Route path="/" element={<NewsPage />} />
+      <Route path="/:category" element={<NewsPage />} />
+    </Routes>
   );
 };
 export default App;
